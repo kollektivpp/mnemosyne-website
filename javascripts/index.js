40e684332746ea3be4c87b1a8771e42ac92cfb68
Thomas Fett <thomasfett@thomass-mbp.fritz.box>
2014-10-08 23:12:09 +0200

    backup commit; Finished responsive behavior.

--- javascripts/index.js
+++ javascripts/index.js
@@ -11,3 +11,18 @@ document.querySelector( '.button-info' ).addEventListener( 'click', function(eve
         footer.classList.add('hidden');
     }
 });
+
+window.onscroll = function(event) {
+    var logo = document.querySelector( '.logo' );
+
+    if (window.pageYOffset > 0) {
+        if (!logo.classList.contains('small')) {
+            logo.classList.add('small');
+        }   
+    } else {
+        if (logo.classList.contains('small')) {
+            logo.classList.remove('small');
+        }   
+    }
+
+};
